1ddf63294d6ee8d158a9ad504bb85d5882dfb327
Aravindh sai <nallamothu196@gmail.com>
2025-04-08 23:09:39 +0530

    Added Task 6 as a regular folder

---
+++ Task 6/script.js
@@ -0,0 +1,44 @@
+
+let emailInputs = document.querySelectorAll('.inp');
+let getStartedButtons = document.querySelectorAll('.start');
+
+getStartedButtons.forEach((btn, index) => {
+    btn.addEventListener('click', function () {
+        let emailValue = emailInputs[index].value.trim();
+
+        if (emailValue === '') {
+            alert("Please enter your email address.");
+        } else {
+            alert("Thanks! We'll contact you at: " + emailValue);
+        }
+    });
+});
+
+
+let allQuestions = document.querySelectorAll('.ques');
+
+allQuestions.forEach(function (singleQuestion) {
+    singleQuestion.addEventListener('click', function () {
+        let existingAnswer = singleQuestion.querySelector('.answer-text');
+
+        if (existingAnswer) {
+            existingAnswer.remove(); 
+        } else {
+            let answerBox = document.createElement('div');
+            answerBox.classList.add('answer-text');
+            answerBox.style.padding = "10px 25px";
+            answerBox.style.fontSize = "18px";
+            answerBox.style.color = "white";
+            answerBox.innerText = "This is a sample answer for the question. You can replace this with real content.";
+
+            singleQuestion.appendChild(answerBox);
+        }
+    });
+});
+
+
+let signInButton = document.querySelector('.sign-btn');
+
+signInButton.addEventListener('click', function () {
+    alert("Redirecting to Sign In page...");
+});
